f24df839b2b7492e6aaea6a546544d640b087d74
milka77 <krisztian.keseru@gmail.com>
2020-03-28 11:29:44 +0000

    fixed bug in game logik cant click the same card anymore, added info and done message to index.html and style.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,102 @@
 // Select HTML elements
-let date = document.getElementById('date');
-let card = document.querySelectorAll('.card');
-let footerYear = document.getElementById('year');
+const date = document.getElementById('date');
+const card = document.querySelectorAll('.card');
+const footerYear = document.getElementById('year');
+const info = document.getElementById('info-msg');
+const done = document.getElementById('done-msg');
+
 
 // Show todays date
-let options = {day:'numeric', weekday:'long', month:'long',};
-let currentDate = new Date();
+const options = {day:'numeric', weekday:'long', month:'long'};
+const currentDate = new Date();
 
 date.innerHTML = currentDate.toLocaleDateString('en-US', options);
 
 // Show current year in the footer
 footerYear.innerHTML = currentDate.getFullYear();
 
+// Player name
+function playerName() {
+    var name = document.getElementById('name-input').value;
+
+    if (name != null) {
+        document.getElementById('player').innerHTML = `Welcome ${name}, and have fun.`;
+    }
+}
+ 
+
 // Memory card game
 let flippedCard = false;
 let firstCard, secondCard;
 let gameLock = false;
+let pairs = 15;
+
+card.forEach(cards => cards.addEventListener('click', flipOver));
 
 function flipOver(){
     if (gameLock) return;
+    if (this === firstCard) return;
     
     this.classList.add('flip');
 
-    if(!flippedCard){
+    if (!flippedCard) {
         // The first click, card
         flippedCard = true;
         firstCard = this;
-        console.log('im the first card', flippedCard, firstCard);
-    }else{
+        
+    } else {
         // The second click, card
         flippedCard = false;
         secondCard = this;
-        console.log('im the second card', flippedCard, secondCard);
+        
         // Checking cards match? 
-        console.log(firstCard.dataset.carddata);
-        console.log(secondCard.dataset.carddata);
-        if(firstCard.dataset.carddata === secondCard.dataset.carddata){
+        
+        if (firstCard.dataset.carddata === secondCard.dataset.carddata) {
             // It's a match
-            firstCard.removeEventListener('click', flipOver);
-            firstCard.classList.add('bg_green');
-            secondCard.removeEventListener('click', flipOver);
-            secondCard.classList.add('bg_green');
-        }else{ 
+           match()
+
+        } else { 
             // Not a match
-            gameLock = true;
-            firstCard.classList.add('bg_red');
-            secondCard.classList.add('bg_red');
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                firstCard.classList.remove('bg_red');
-                secondCard.classList.remove('flip');
-                secondCard.classList.remove('bg_red');
-                gameLock = false;
-            }, 1600);
+          flipBack()
         }
+    }
+
+    // Show the end game screen
+    if (pairs <= 0){
+        win.style.display = 'block';
     }
 }
 
+function match(){
+    firstCard.removeEventListener('click', flipOver);
+    firstCard.classList.add('bg_green');
+    secondCard.removeEventListener('click', flipOver);
+    secondCard.classList.add('bg_green');
+    pairs --;
+}
 
-function reset(){
+function flipBack() {
+    gameLock = true;
+    firstCard.classList.add('bg_red');
+    secondCard.classList.add('bg_red');
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        firstCard.classList.remove('bg_red');
+        secondCard.classList.remove('flip');
+        secondCard.classList.remove('bg_red');
+        gameLock = false;
+    }, 1600);
+}
+
+function windowClose() {
+    win.style.display = 'none';
+}
+
+function reset() {
     flippedCard = false;
     firstCard, secondCard = [null, null];
+    pairs = 15;
     card.forEach(cards => cards.addEventListener('click', flipOver));
 }
- card.forEach(cards => cards.addEventListener('click', flipOver));
+
+
